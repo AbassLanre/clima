944a8ef315ee741bfd9613c7b2f9d48926046b4f
Abass <kalanruse@gmail.com>
2020-09-28 00:57:40 +0100

    update getLocation

--- lib/ui/screens/loading_screen.dart
+++ lib/ui/screens/loading_screen.dart
@@ -8,9 +8,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation(){
+ void getLocation() async {
+//    print("hey1");
+//    Position position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+//    print("hey");
+//    print(position);
 
+      StreamSubscription<LocationResult> subscription = await
+          Geolocation.currentLocation(accuracy: LocationAccuracy.best)
+              .listen((result) {
+        if (result.isSuccessful) {
+          double latitude = result.location.latitude;
+          double longitude = result.location.longitude;
+          print({latitude, longitude});
+        }
+      });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
