503e48ec7949645e49622ff12eb7b252bdcd0441
Abass <kalanruse@gmail.com>
2020-10-05 03:16:54 +0100

    updated fontSize

--- lib/ui/utilities/constants.dart
+++ lib/ui/utilities/constants.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 70.0,
 );
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 50.0,
 );
 
 const kButtonTextStyle = TextStyle(
@@ -16,5 +16,5 @@ const kButtonTextStyle = TextStyle(
 );
 
 const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+  fontSize: 70.0,
 );
